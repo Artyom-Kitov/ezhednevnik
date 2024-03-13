cfb620387fa4018e3c5e9ef018ee163f9a92d692
Mikhail Sartakov <m.sartakov@g.nsu.ru>
2024-03-13 12:51:30 +0700

    initial backend commit

---
+++ backend/src/main/java/ru/nsu/fit/ezhednevnik/EzhednevnikApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.fit.ezhednevnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzhednevnikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzhednevnikApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ru/nsu/fit/ezhednevnik/EzhednevnikApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.fit.ezhednevnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzhednevnikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
