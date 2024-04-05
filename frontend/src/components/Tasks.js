cd976e8d8c0ed5ce8bdbc9c9763210c116eb5879
anton-ableton <a.nazarov3@g.nsu.ru>
2024-04-05 13:56:54 +0700

    EZD-9: added frontend

---
+++ frontend/src/components/Tasks.js
@@ -0,0 +1,25 @@
+import React from "react"
+import Task from "./Task"
+
+class Tasks extends React.Component {
+  render() {
+    if (this.props.tasks.length > 0) {
+      return (
+        <div>
+          {this.props.tasks.map((el) => (
+            <Task onEdit={this.props.onEdit} onDelete={this.props.onDelete} key={el.id} task={el}/>
+          ))}
+        </div>
+      )
+    }
+    return (
+      <div className="notask">
+        <h3>No tasks</h3>
+      </div> 
+    )
+
+  }
+
+}
+
+export default Tasks
